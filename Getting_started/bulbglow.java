fcc0c7569479aea1c01798eb0fbe1d1087b600f0
ishangupta17 <43989286+ishangupta17@users.noreply.github.com>
2021-05-15 23:23:03 +0530

    Add files via upload

---
+++ Getting_started/bulbglow.java
@@ -0,0 +1,12 @@
+import java.util.*;
+  
+  public class bulbglow{
+  
+  public static void main(String[] args) {
+    Scanner s =new Scanner(System.in);
+    int n=s.nextInt();
+    for(int i=1;i*i<n;i++){
+        System.out.println(i*i);
+    }
+   }
+  }
